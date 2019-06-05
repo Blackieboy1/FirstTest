00b73a4383e1e3e6ecf05352dbb0a826e372cad1
Blackieboy1 <rsanyaolu@hotmail.com>
2019-06-05 14:48:48 +0100

    First Commit

---
+++ Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Comment single comment
+		
+		int number=7; // number 
+		
+		
+		/*  Multiline comment
+		 
+		   Hello
+		   Hello
+		   
+		   
+		   */
+		
+		System.out.println("Hello My first Java Code"); // Print statement
+		
+		
+
+	}
+
+}
